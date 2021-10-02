63d8e794a83784da3bdec91ddbc38ab882ef9e93
ShradhaR25 <79851303+ShradhaR25@users.noreply.github.com>
2021-10-02 23:16:25 +0530

    changemove

--- FinalProject/Assets/script1/NoteAppear.cs
+++ FinalProject/Assets/script1/NoteAppear.cs
@@ -3,26 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NoteAppear : MonoBehaviour
+public class Gate2 : MonoBehaviour
 {
-    [SerializeField]
-    private Image _noteImage ;
 
+    public MouseLook mouseLook;
+    [SerializeField]
+    private GameObject Door1Img;
+
+    // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _noteImage.enabled = true;
+            mouseLook.enabled = false;
+            ShowCursor();
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                HideCursor();
+            }
         }
-
     }
-
 
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _noteImage.enabled = false;
+            mouseLook.enabled = true;
+            HideCursor();
         }
+    }
+
+
+    public void ShowCursor()
+    {
+
+        Door1Img.SetActive(true);
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void HideCursor()
+    {
+
+        Door1Img.SetActive(false);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
