4288c96f9cc4045a808d7ee865dbbb5925bdcd21
Abhab24 <abha155btece20@igdtuw.ac.in>
2021-10-02 23:27:17 +0530

    pop3

--- FinalProject/Assets/script1/NoteAppear.cs
+++ FinalProject/Assets/script1/NoteAppear.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Gate2 : MonoBehaviour
+public class NoteAppear : MonoBehaviour
 {
 
     public MouseLook mouseLook;
